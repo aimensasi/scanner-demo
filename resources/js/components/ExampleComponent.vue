23e33827e74ae1f5fcd49f521edf2e859cc9b022
AIMEN .S.A.SASI <aimen.s.a.sasi@AIMENs-MacBook-Air-2.local>
2020-04-25 22:08:06 +0800

    added the demo of the scanner

---
+++ resources/js/components/ExampleComponent.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <StreamBarcodeReader
+    v-if="scan"
+    @decode="onDecode"
+    @loaded="onLoaded"
+    ></StreamBarcodeReader>
+    <button class="btn btn-primary" @click="() => scan = true" v-if="!scan">Scan</button>
+  </div>
+</template>
+
+<script>
+import { StreamBarcodeReader } from "vue-barcode-reader";
+
+export default {
+    components: {
+        StreamBarcodeReader,
+    },
+    data: function(){
+        return { 
+            scan: false,
+        };
+    },
+    mounted(){
+        console.log("Mounted...");
+    },
+    methods: {
+        onDecode: function(result){
+            console.log(result);
+            this.scan = false;
+        },
+            onLoaded: function(result){
+            console.log(result)
+        }
+    }
+}
+</script>
